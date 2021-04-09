afa2eaf9bf487c1b4df54ca6deebc534de5b29fa
dies-iris <51315734+dies-iris@users.noreply.github.com>
2021-04-09 14:02:13 +0200

    added bll getarticles

--- src/org/eni/encheres/bll/encheres/EnchereManager.java
+++ src/org/eni/encheres/bll/encheres/EnchereManager.java
@@ -1,5 +1,15 @@
 package org.eni.encheres.bll.encheres;
 
-public interface EnchereManager{
+import java.util.ArrayList;
 
+import org.eni.encheres.BusinessException;
+import org.eni.encheres.bo.*;
+
+public interface EnchereManager{
+	
+	public ArticleVendu addArticle(Utilisateur vendeur, String nom, String description, Categorie categorie, String date_debut, String date_fin, int miseAPrix, String rue, String code_postal, String ville) throws BusinessException;
+	public ArticleVendu updateArticle(ArticleVendu article) throws BusinessException;
+	public void deleteArticle(int noArticle) throws BusinessException;
+	public ArrayList<ArticleVendu> getArticles() throws BusinessException;
+	public Enchere addEnchere (Utilisateur utilisateur, int no_article, int montant_enchere) throws BusinessException;
 }
--- src/org/eni/encheres/bll/encheres/EnchereManagerImpl.java
+++ src/org/eni/encheres/bll/encheres/EnchereManagerImpl.java
@@ -1,5 +1,49 @@
 package org.eni.encheres.bll.encheres;
 
+import java.util.ArrayList;
+
+import org.eni.encheres.BusinessException;
+import org.eni.encheres.bo.ArticleVendu;
+import org.eni.encheres.bo.Categorie;
+import org.eni.encheres.bo.Enchere;
+import org.eni.encheres.bo.Utilisateur;
+import org.eni.encheres.dal.DAOFactory;
+import org.eni.encheres.dal.EncheresDAO;
+
 public class EnchereManagerImpl implements EnchereManager{
+	
+	private EncheresDAO dao = DAOFactory.getInstance(); 
+
+	@Override
+	public ArticleVendu addArticle(Utilisateur vendeur, String nom, String description, Categorie categorie,
+			String date_debut, String date_fin, int miseAPrix, String rue, String code_postal, String ville)
+			throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArticleVendu updateArticle(ArticleVendu article) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteArticle(int noArticle) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Enchere addEnchere(Utilisateur utilisateur, int no_article, int montant_enchere) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<ArticleVendu> getArticles() throws BusinessException {
+		ArrayList<ArticleVendu> articles = dao.selectAllArticles();
+		return articles;
+	}
 
 }
